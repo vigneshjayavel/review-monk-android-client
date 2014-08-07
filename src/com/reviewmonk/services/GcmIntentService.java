6e1cf692c96c46e0cfe59cf9ec083193e612c1bb
karthickpdy <karthickpdy@gmail.com>
2014-08-08 04:18:30 +0530

    Push completed activity to be added

--- src/com/reviewmonk/services/GcmIntentService.java
+++ src/com/reviewmonk/services/GcmIntentService.java
@@ -15,51 +15,70 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.reviewmonk.R;
 import com.reviewmonk.activity.Push;
 
-public class GcmIntentService extends IntentService
-{     public static final int NOTIFICATION_ID = 1;     private NotificationManager mNotificationManager;     private final static String TAG = "GcmIntentService";     public GcmIntentService() {
-     super("GcmIntentService");     
-     }     @Override
-     protected void onHandleIntent(Intent intent) {
-          Bundle extras = intent.getExtras();
-          Log.d(TAG, "Notification Data Json :" + extras.getString("my_message"));
+public class GcmIntentService extends IntentService {
+	public static final int NOTIFICATION_ID = 1;
+	private NotificationManager mNotificationManager;
+	private final static String TAG = "GcmIntentService";
 
-          GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
-          String messageType = gcm.getMessageType(intent);          if (!extras.isEmpty()) {          if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR
-               .equals(messageType)) {
-               sendNotification("Send error: " + extras.toString());
-          } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED
-          .equals(messageType)) {
-          sendNotification("Deleted messages on server: "
-          + extras.toString());          // If it's a regular GCM message, do some work.
-          } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE
-          .equals(messageType)) {
-          // This loop represents the service doing some work.
-          for (int i = 0; i < 5; i++) {
-               Log.d(TAG," Working... " + (i + 1) + "/5 @ "
-               + SystemClock.elapsedRealtime());               try {
-                    Thread.sleep(5000);
-               } catch (InterruptedException e) {
-               }
-             }
-             Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
-             sendNotification(extras.getString("my_message"));
-           }
-        }        // Release the wake lock provided by the WakefulBroadcastReceiver.
-        GcmBroadcastReceiver.completeWakefulIntent(intent);
-     }     // Put the message into a notification and post it.
-     // This is just one simple example of what you might choose to do with
-     // a GCM message.
-     private void sendNotification(String msg) {          mNotificationManager = (NotificationManager) this
-          .getSystemService(Context.NOTIFICATION_SERVICE);
-          PendingIntent contentIntent = PendingIntent.getActivity(this, 0,    new Intent(this, Push.class), 0);
+	public GcmIntentService() {
+		super("GcmIntentService");
+	}
 
-          NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(          this)
-          .setSmallIcon(R.drawable.ic_launcher)
-          .setContentTitle("Ocutag Snap")
-          .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-          .setContentText(msg)
-          .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Bundle extras = intent.getExtras();
+		Log.d(TAG, "Notification Data Json :" + extras.getString("my_message"));
 
-          mBuilder.setContentIntent(contentIntent);          mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-     }
+		GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+		String messageType = gcm.getMessageType(intent);
+		if (!extras.isEmpty()) {
+			if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR
+					.equals(messageType)) {
+				sendNotification("Send error: " + extras.toString());
+			} else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED
+					.equals(messageType)) {
+				sendNotification("Deleted messages on server: "
+						+ extras.toString()); // If it's a regular GCM message,
+												// do some work.
+			} else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE
+					.equals(messageType)) {
+				// This loop represents the service doing some work.
+				for (int i = 0; i < 5; i++) {
+					Log.d(TAG,
+							" Working... " + (i + 1) + "/5 @ "
+									+ SystemClock.elapsedRealtime());
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+					}
+				}
+				Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+				Log.i(TAG, extras.toString());
+				sendNotification(extras.getString("message_text"));
+			}
+		} // Release the wake lock provided by the WakefulBroadcastReceiver.
+		GcmBroadcastReceiver.completeWakefulIntent(intent);
+	} // Put the message into a notification and post it.
+
+	// This is just one simple example of what you might choose to do with
+	// a GCM message.
+	private void sendNotification(String msg) {
+		mNotificationManager = (NotificationManager) this
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+				new Intent(this, Push.class), 0);
+
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+				this)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Review Monk Notification")
+				.setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+				.setContentText(msg)
+				.setDefaults(
+						Notification.DEFAULT_SOUND
+								| Notification.DEFAULT_VIBRATE);
+
+		mBuilder.setContentIntent(contentIntent);
+		mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+	}
 }
